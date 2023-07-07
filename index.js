684d45556d35e995289de77363763120b1e49e4c
Levy <edgarlevy81@gmail.com>
2023-07-08 01:21:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+//saturdayFun
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    }
+    saturdayFun()
+ 
+ //mondayWork   
+const mondayWork = function(action="go to the office"){
+    return `This Monday, I will ${action}.` 
+    }
+    mondayWork()    
+
+//wrapAdjective-hard worker
+function wrapAdjective (sign="*") {
+    return function(character="a hard worker"){
+    return `You are ${sign}${character}${sign}!`
+        }
+    }
+//wrapAdjective-dedicated
+function wrapAdjective (sign="||") {
+    return function(character="a dedicated programmer"){
+    return `You are ${sign}${character}${sign}!`
+            }
+        }
+     
